f9dc1ee8ea1bcf8236075a5dc19e740d7610ba9a
Anaminus <anaminus@no-email>
2014-02-20 03:59:15 -0600

    combined create mode into deploy mode (closes #3)

--- rbxplugin.go
+++ rbxplugin.go
@@ -9,7 +9,6 @@ import (
 	flags "github.com/jessevdk/go-flags"
 	"io"
 	"net/http"
-	"net/http/cookiejar"
 	"net/url"
 	"os"
 	"strconv"
@@ -19,13 +18,12 @@ type Opts struct {
 	Help        bool   `short:"h" long:"help"        optional:"true"                                              description:"Shows this help message."`
 	Build       bool   `short:"b" long:"build"       optional:"true"                                              description:"Builds the plugin to the output file."`
 	Deploy      bool   `short:"d" long:"deploy"      optional:"true"                                              description:"Deploys the results of the build to a plugin asset on the ROBLOX website."`
-	Create      bool   `short:"c" long:"create"      optional:"true"                                              description:"Creates a new plugin asset on the ROBLOX website."`
 	Input       string `short:"i" long:"input"       optional:"true" value-name:"[dir]"    default:"."            description:"The directory to process into a plugin."`
 	Output      string `short:"o" long:"output"      optional:"true" value-name:"[file]"   default:"./build.rbxm" description:"The rbxm file to output to."`
 	Username    string `short:"u" long:"username"    optional:"true" value-name:"[string]"                        description:"The username to use for logging in when creating or deploying."`
 	Password    string `short:"p" long:"password"    optional:"true" value-name:"[string]"                        description:"The password to use for logging in when creating or deploying."`
 	Asset       int64  `short:"a" long:"asset"       optional:"true" value-name:"[int]"                           description:"The id of the asset to deploy to."`
-	Name        string `short:"n" long:"name"        optional:"true" value-name:"[string]" default:"Plugin"       description:"The plugin name used when creating or deploying."`
+	Name        string `short:"n" long:"name"        optional:"true" value-name:"[string]"                        description:"The plugin name used when creating or deploying."`
 	Description string `short:"m" long:"description" optional:"true" value-name:"[string]"                        description:"The plugin description used when creating or deploying."`
 }
 
@@ -42,64 +40,47 @@ func Build(opts Opts) (results io.Reader, err error) {
 	return bytes.NewReader(buildResults), nil
 }
 
-func Deploy(opts Opts) (err error) {
-	if opts.Username == "" {
-		return errors.New("Deployment requires a username to be specified")
-	}
-
-	if opts.Password == "" {
-		return errors.New("Deployment requires a password to be specified")
-	}
-
-	if opts.Asset == 0 {
-		return errors.New("Deployment requires an asset id to be specified")
-	}
-
+func Update(opts Opts, client *http.Client) (err error) {
 	results, err := Build(opts)
 	if err != nil {
-		return err
-	}
-
-	client := http.DefaultClient
-	if client.Jar == nil {
-		client.Jar, _ = cookiejar.New(&cookiejar.Options{})
-	}
-	err = rbxweb.Login(client, opts.Username, opts.Password)
-	if err != nil && err != rbxweb.ErrLoggedIn {
 		return err
 	}
 
 	info := url.Values{
 		"type":    {"Plugin"},
 		"assetid": {strconv.FormatInt(opts.Asset, 10)},
-		//"name":          {opts.Name},
-		//"description":   {opts.Description},
-		//"isPublic":      {"True"},
-		//"genreTypeId":   {"1"},
-		//"allowComments": {"True"},
 	}
 
 	_, err = asset.Upload(client, results, info)
-	return
-}
 
-func Create(opts Opts) (err error) {
-	if opts.Username == "" {
-		return errors.New("Creating requires a username to be specified")
-	}
+	if opts.Name != "" || opts.Description != "" {
+		params := url.Values{
+			"__RequestVerificationToken": {},
+			"Name":                {},
+			"Description":         {},
+			"ThumbnailYoutubeUrl": {},
+		}
+		if opts.Name != "" {
+			params.Set("Name", opts.Name)
+		}
+		if opts.Description != "" {
+			params.Set("Description", opts.Description)
+		}
 
-	if opts.Password == "" {
-		return errors.New("Creating requires a password to be specified")
-	}
-
-	results, err := Build(opts)
-	if err != nil {
+		err = rbxweb.DoRawPost(client, `http://www.roblox.com/plugins/`+strconv.FormatInt(opts.Asset, 10)+`/update`, params)
+		// rbxweb's error handling needs to be improved
+		if err.Error() == "301: Moved Permanently" {
+			return nil
+		}
 		return err
 	}
 
-	client := http.DefaultClient
-	err = rbxweb.Login(client, opts.Username, opts.Password)
-	if err != nil && err != rbxweb.ErrLoggedIn {
+	return err
+}
+
+func Create(opts Opts, client *http.Client) (err error) {
+	results, err := Build(opts)
+	if err != nil {
 		return err
 	}
 
@@ -114,7 +95,29 @@ func Create(opts Opts) (err error) {
 	}
 
 	_, err = asset.Upload(client, results, info)
-	return
+	return err
+}
+
+func Deploy(opts Opts) (err error) {
+	if opts.Username == "" {
+		return errors.New("Deployment requires a username to be specified")
+	}
+
+	if opts.Password == "" {
+		return errors.New("Deployment requires a password to be specified")
+	}
+
+	client := http.DefaultClient
+	err = rbxweb.Login(client, opts.Username, opts.Password)
+	if err != nil && err != rbxweb.ErrLoggedIn {
+		return err
+	}
+
+	if opts.Asset == 0 {
+		return Create(opts, client)
+	} else {
+		return Update(opts, client)
+	}
 }
 
 func assert(err error) {
@@ -155,12 +158,6 @@ func main() {
 		assert(err)
 		io.Copy(f, results)
 		f.Close()
-	}
-
-	if opts.Create {
-		modeActive = true
-		err := Create(opts)
-		assert(err)
 	}
 
 	if opts.Deploy {
